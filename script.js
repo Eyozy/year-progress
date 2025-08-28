b7025aa94ad9d315dc6deef9e9a351674d5bb2fe
Eyozy <110926935+Eyozy@users.noreply.github.com>
2025-08-28 23:45:57 +0800

    fix: Change vertical alignment from baseline to middle

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ document.addEventListener("DOMContentLoaded", () => {
                   lineHeight: "normal",
                   color: originalStyle.color,
                   textAlign: originalStyle.textAlign,
-                  verticalAlign: "baseline",
+                  verticalAlign: "middle",
                 });
               });
           }
